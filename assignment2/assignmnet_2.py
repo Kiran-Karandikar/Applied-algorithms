75231f82b66532ff2adb75a42302839915a37c20
Kiran Karandikar <no-reply-kiran-karandikar@git.com>
2021-09-24 20:52:03 -0400

    Assignment 2 - WIP
    Took 25 minutes

--- assignment2/assignmnet_2.py
+++ assignment2/assignmnet_2.py
@@ -171,15 +171,27 @@ def main():
 	print(w * x * y * z)
 	print((1 - 3j) * (-1 + 3j) * (1 + 3j) * (-1 - 3j))
 	print("Problem 4: Running time")
-	data_points = []
+	data_points_avg1 = []
+	data_points_avg2 = []
+	data_points_avg3 = []
 	data_size = []
-	for i in range(1000):
+	for i in range(40):
 		s = [randint(0, i) for j in range(i)]
 		t = Timer(lambda: average1(s))
 		run_time = t.timeit()
-		data_points.append(run_time)
+		data_points_avg1.append(run_time)
+		t = Timer(lambda: average2(s))
+		run_time = t.timeit()
+		data_points_avg2.append(run_time)
+		t = Timer(lambda: average3(s))
+		run_time = t.timeit()
+		data_points_avg3.append(run_time)
 		data_size.append(i + 1)
 	fig = plt.figure()
+	# naming the x-axis
+	plt.xlabel("Input Size")
+	# naming the y-axis
+	plt.ylabel("Running time")
 	ax = fig.add_subplot(1, 1, 1)
 	# spine placement data centered
 	ax.spines ['left'].set_position(('data', 0.0))
@@ -188,14 +200,39 @@ def main():
 	ax.spines ['top'].set_color('none')
 	ax.xaxis.set_ticks_position("bottom")
 	ax.yaxis.set_ticks_position("left")
-	plt.title("f'(x) and f''(x)")
-	plt.plot(data_points, data_size, "r")
+	# ax.set_xticks(list(range(0, 10, 2)))
+	plt.title("log(runtime) and log(input size)")
+	ax.legend(["average1", "average2", "average3"])
+	plt.loglog(data_size, data_points_avg1, "r")
+	plt.loglog(data_size, data_points_avg2, "g")
+	plt.loglog(data_size, data_points_avg3, "b")
+	plt.annotate("Input size V/s Running time", xy=(1.01, -2.15))
 	plt.show()
-	# s = [randrange(0, 10000) for i in range(10000)]
-	# t = Timer(lambda: algorithm1(s))
-	# print(t.timeit())
-	# todo - run similar to this
+	run_time_alg1 = 0
+	run_time_alg2 = 0
+	run_time_alg3 = 0
 	print("Problem 5: Time analysis")
+	for i in range(100):
+		s = [randint(0, i) for j in range(i)]
+		t = Timer(lambda: algorithm1(s))
+		if run_time_alg1 < 45:
+			run_time_alg1 = t.timeit()
+		else:
+			print("Max Input size for algorithm 1 is :{}".format(i))
+		t = Timer(lambda: algorithm2(s))
+		if run_time_alg2 < 45:
+			run_time_alg2 = t.timeit()
+		else:
+			print("Max Input size for algorithm 2 is :{}".format(i))
+		t = Timer(lambda: algorithm3(s, 0, i))
+		if run_time_alg3 < 45:
+			run_time_alg3 = t.timeit()
+		else:
+			print("Max Input size for algorithm 3 is :{}".format(i))
+
+	print("Run times for alg1 :{}".format(run_time_alg1))
+	print("Run times for alg2 :{}".format(run_time_alg2))
+	print("Run times for alg3 :{}".format(run_time_alg3))
 
 
 if __name__ == '__main__':
